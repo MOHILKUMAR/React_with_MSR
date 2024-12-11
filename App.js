5ac07ddbad6e595d6e460329d5bece1d2527d792
user.Mohil Kumar <user.mohil4280@gmail.com>
2024-12-12 04:31:04 +0530

    episode 2

--- App.js
+++ App.js
@@ -87,19 +87,23 @@
 *
 * */
 // core of react 
-// const parent = React.createElement("div", { id: "parent" }, [
-//   React.createElement("div", { id: "child1" }, [
-//     React.createElement("h1", {}, "I'm  H1 tag"),
-//     React.createElement("h2", {}, "I'm  H2 tag"),
-//   ]),
-//   React.createElement("div", { id: "child2" }, [
-//     React.createElement("h1", {}, "I'm  H1 tag"),
-//     React.createElement("h2", {}, "I'm  H2 tag"),
-//   ]),
-// ]);
 
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(parent);
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child1" }, [
+    React.createElement("h1", {}, "This is namaste React"),
+    React.createElement("h2", {}, "I'm  mohil kumar"),
+  ]),
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I'm  H sfsf 3tag"),
+    React.createElement("h2", {}, "I'm  H2 tag"),
+  ]),
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
 
 
 //JSX 
--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 #Namaste React 
+
+#parcel
+-Dev Bulid
+-local server
+-HMR = hot Module Replacement
+Bundling
+File Watcher Algorithm - C++
+Minify Code
+Cleaning our code
+Dev and production build
+Igniting Our App! Namaste-React 
+Super fast build algorithm
+Image Optimization
+Caching while development
+Compression
+Compatible with older browser versions
+Https on dev
+Image Optimization
+Port No
+Consistency Hashing Algorithm
+Zero Config
+Tree Shaking
